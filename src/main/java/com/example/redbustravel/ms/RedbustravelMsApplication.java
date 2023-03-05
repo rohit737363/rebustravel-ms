3c9b58cbd977296266e58a0b427a2c8b2e5a9480
rohit737363 <chougalerohit73@gmail.com>
2023-03-05 08:30:51 +0530

    codepush

---
+++ src/main/java/com/example/redbustravel/ms/RedbustravelMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.redbustravel.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbustravelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbustravelMsApplication.class, args);
+	}
+
+}
